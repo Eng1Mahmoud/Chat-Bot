10677f7f1fc1b5d9a9e74dda9a0909a5a0a56c23
Eng1Mahmoud <mahmoudmohamedabdelaal1@gmail.com>
2023-03-18 18:59:01 +0200

    time delay

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
-const PORT = process.env.PORT 
+const PORT = process.env.PORT;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const telegramToken = process.env.telegram_token;
 const bot = new TelegramBot(telegramToken, { polling: true });
@@ -17,23 +17,25 @@ bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const message = msg.text;
 
-
   bot.sendMessage(
     process.env.adminId,
     `رسالة من \n${msg.from.first_name} \n username is ${msg.from.username}\n message is ${msg.text}`
   );
-  bot.sendChatAction(msg.from.id, "typing")
+  bot.sendChatAction(msg.from.id, "typing");
   const completion = openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: message }],
   });
-  completion.then((res) =>
-    bot.sendMessage(msg.from.id, res.data.choices[0].message.content, {
-      reply_markup: {
-        remove_keyboard: true,
-      },
-    })
-  );
+  completion.then((res) => {
+    //delay before sending the bot's response
+    setTimeout(() => {
+      bot.sendMessage(msg.chat.id, res.data.choices[0].message.content, {
+        reply_markup: {
+          remove_keyboard: true,
+        },
+      });
+    }, 1000); // delay of 1 second
+  });
 });
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
